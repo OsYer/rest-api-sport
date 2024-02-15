dfc69947adf092093cebb451f5a85bb8b3f8e882
binacarl1 <20210720@uthh.edu.mx>
2024-02-15 13:12:53 -0600

    Hasheo

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -1,5 +1,212 @@
+// import { getConnection, querysUsers, sql } from "../database";
+// const bcrypt = require('bcrypt');
+
+// export const getUsers = async (req, res) => {
+//   try {
+//     const pool = await getConnection();
+//     const result = await pool.request().query(querysUsers.getAllUsers);
+//     res.json(result.recordset);
+//   } catch (error) {
+//     res.status(500);
+//     res.send(error.message);
+//   }
+// };
+
+// export const createNewUser = async (req, res) => {
+//   const { nombre, primerApellido, segundoApellido, fechaNacimiento, genero, correoElectronico, telefono, contrasena, direccion } = req.body;
+
+//   if ((nombre == null || primerApellido == null || segundoApellido == null || fechaNacimiento == null || genero == null || correoElectronico == null || telefono == null || contrasena == null || direccion == null) ||
+//     (nombre === '' || primerApellido === '' || segundoApellido === '' || fechaNacimiento === '' || genero === '' || correoElectronico === '' || telefono == null || contrasena === '' || direccion === '')) {
+//     return res.status(400).json({ msg: "Solicitud incorrecta. Por favor complete todos los campos" });
+//   }
+
+//   try {
+//     const pool = await getConnection();
+
+//     const existingUser = await pool
+//       .request()
+//       .input("correoElectronico", sql.VarChar, correoElectronico)
+//       .query(querysUsers.getUserByEmail);
+//     const existingUserTelephone = await pool
+//       .request()
+//       .input("telefono", sql.VarChar, telefono)
+//       .query(querysUsers.getUserByTelephone);
+
+//     if (existingUser.recordset.length > 0) {
+//       return res.status(409).json({ msg: "Un usuario con ese correo ya existe" });
+//     }
+    
+//     if (existingUserTelephone.recordset.length > 0) {
+//       return res.status(409).json({ msg: "Un usuario con ese teléfono ya existe" });
+//     }
+
+//     const hashedPassword = await bcrypt.hash(contrasena, 10);
+
+//     await pool
+//       .request()
+//       .input("nombre", sql.VarChar, nombre)
+//       .input("primerApellido", sql.VarChar, primerApellido)
+//       .input("segundoApellido", sql.VarChar, segundoApellido)
+//       .input("fechaNacimiento", sql.Date, fechaNacimiento)
+//       .input("genero", sql.VarChar, genero)
+//       .input("correoElectronico", sql.VarChar, correoElectronico)
+//       .input("telefono", sql.BigInt, telefono)
+//       .input("contraseña", sql.VarChar, hashedPassword) // Se almacena la contraseña hasheada
+//       .input("direccion", sql.VarChar, direccion)
+//       .query(querysUsers.addNewUser);
+
+//     res.json({ nombre, primerApellido, segundoApellido, fechaNacimiento, genero, correoElectronico, telefono, contrasena: hashedPassword, direccion });
+//   } catch (error) {
+//     res.status(500).send(error.message);
+//   }
+// };
+
+
+
+// export const getUserById = async (req, res) => {
+//   try {
+//     const pool = await getConnection();
+//     const result = await pool
+//       .request()
+//       .input("IdUsuario", req.params.id)
+//       .query(querysUsers.getUserById);
+//     return res.json(result.recordset[0]);
+//   } catch (error) {
+//     res.status(500);
+//     res.send(error.message);
+//   }
+// };
+
+// export const getUserByEmail = async (req, res) => {
+//   const { email } = req.params;
+
+//   if (email == null || email === '') {
+//     return res.status(400).json({ msg: 'Solicitud incorrecta. Proporcione un correo electrónico' });
+//   }
+
+//   try {
+//     const pool = await getConnection();
+//     const result = await pool
+//       .request()
+//       .input('correoElectronico', sql.VarChar, email)
+//       .query(querysUsers.getUserByEmail);
+
+//     if (result.recordset.length === 0) {
+//       return res.status(404).json({ msg: 'Usuario no encontrado' });
+//     }
+    
+//     const user = result.recordset[0];
+//     return res.status(200).json(user);
+
+//   } catch (error) {
+//     res.status(500).json({ msg: 'Error interno del servidor' });
+//   }
+// };
+
+// export const deleteUserById = async (req, res) => {
+//   try {
+//     const pool = await getConnection();
+
+//     const result = await pool
+//       .request()
+//       .input("IdUsuario", req.params.id)
+//       .query(querysUsers.deleteUser);
+
+//     if (result.rowsAffected[0] === 0) return res.sendStatus(404);
+
+//     return res.sendStatus(204);
+//   } catch (error) {
+//     res.status(500);
+//     res.send(error.message);
+//   }
+// };
+
+// export const getTotalUsers = async (req, res) => {
+//   const pool = await getConnection();
+
+//   const result = await pool.request().query(querysUsers.getTotalUsers);
+//   res.json(result.recordset[0][""]);
+// };
+
+// export const updateUserById = async (req, res) => {
+//   const { nombre, primerApellido, segundoApellido, fechaNacimiento, genero, correoElectronico, telefono, contrasena, direccion } = req.body;
+
+//   if ((nombre == null || primerApellido == null || segundoApellido == null || fechaNacimiento == null || genero == null || correoElectronico == null || telefono == null || contrasena == null || direccion == null) ||
+//       (nombre == '' || primerApellido == '' || segundoApellido == '' || fechaNacimiento == '' || genero == '' || correoElectronico == '' || telefono == null || contrasena == null || direccion == '')) {
+//     return res.status(400).json({ msg: "Solicitud incorrecta. Por favor complete todos los campos" });
+//   }
+
+//   try {
+//     const pool = await getConnection();
+//     await pool
+//       .request()
+//       .input("nombre", sql.VarChar, nombre)
+//       .input("primerApellido", sql.VarChar, primerApellido)
+//       .input("segundoApellido", sql.VarChar, segundoApellido)
+//       .input("fechaNacimiento", sql.Date, fechaNacimiento)
+//       .input("genero", sql.VarChar, genero)
+//       .input("correoElectronico", sql.VarChar, correoElectronico)
+//       .input("telefono", sql.BigInt, telefono)
+//       .input("contraseña", sql.VarChar, contrasena)
+//       .input("direccion", sql.VarChar, direccion)
+//       .query(querysUsers.updateUserById);
+//     res.json({ nombre, primerApellido, segundoApellido, fechaNacimiento, genero, correoElectronico, telefono, contrasena, direccion });
+//   } catch (error) {
+//     res.status(500);
+//     res.send(error.message);
+//   }
+// };
+
+// export const updatePasswordById = async (req, res) => {
+//   const { contraseña } = req.body;
+
+//   if (contraseña == null) {
+//     return res.status(400).json({ msg: "Solicitud incorrecta. Por favor proporcione una contraseña" });
+//   }
+
+//   try {
+//     const pool = await getConnection();
+
+//     await pool
+//       .request()
+//       .input("contraseña", sql.VarChar, contraseña)
+//       .input("IdUsuario", req.params.id)
+//       .query(querysUsers.updatePasswordById);
+
+//     res.json({ success: true });
+//   } catch (error) {
+//     res.status(500).json({ success: false, error: error.message });
+//   }
+// };
+
+// export const login = async (req, res) => {
+//   const { correoElectronico, contraseña } = req.body;
+
+//   if ((correoElectronico == null || contraseña == null) || (correoElectronico == '' || contraseña == '')) {
+//     return res.status(400).json({ msg: "Solicitud incorrecta. Por favor proporcione tanto el correo electrónico como la contraseña" });
+//   }
+//   try {
+//     const pool = await getConnection();
+
+//     const result = await pool
+//       .request()
+//       .input("correoElectronico", sql.VarChar, correoElectronico)
+//       .input("contraseña", sql.VarChar, contraseña)
+//       .query(querysUsers.login);
+
+//     if (result.recordset.length === 0) {
+//       return res.status(401).json({ msg: "Correo electrónico o contraseña no válidos" });
+//     }
+
+//     const user = result.recordset[0];
+
+//     res.json(user);
+//   } catch (error) {
+//     res.status(500).json({ msg: "Error interno del servidor", error: error.message });
+//   }
+// };
 import { getConnection, querysUsers, sql } from "../database";
-const bcrypt = require('bcrypt');
+import bcrypt from 'bcrypt';
 
 export const getUsers = async (req, res) => {
   try {
@@ -13,55 +220,53 @@ export const getUsers = async (req, res) => {
 };
 
 export const createNewUser = async (req, res) => {
-  const { nombre, primerApellido, segundoApellido, fechaNacimiento, genero, correoElectronico, telefono, contrasena, direccion } = req.body;
+  const { nombre, primerApellido, segundoApellido, correoElectronico, contrasena } = req.body;
 
-  if ((nombre == null || primerApellido == null || segundoApellido == null || fechaNacimiento == null || genero == null || correoElectronico == null || telefono == null || contrasena == null || direccion == null) ||
-    (nombre === '' || primerApellido === '' || segundoApellido === '' || fechaNacimiento === '' || genero === '' || correoElectronico === '' || telefono == null || contrasena === '' || direccion === '')) {
-    return res.status(400).json({ msg: "Solicitud incorrecta. Por favor complete todos los campos" });
+  if (!nombre || !primerApellido || !segundoApellido || !correoElectronico || !contrasena) {
+    return res.status(400).json({ msg: "Por favor complete todos los campos." });
   }
 
   try {
     const pool = await getConnection();
 
-    const existingUser = await pool
+    // Verificar si el correo electrónico ya está registrado en la tabla Credenciales
+    const existingCredential = await pool
       .request()
       .input("correoElectronico", sql.VarChar, correoElectronico)
-      .query(querysUsers.getUserByEmail);
-    const existingUserTelephone = await pool
-      .request()
-      .input("telefono", sql.VarChar, telefono)
-      .query(querysUsers.getUserByTelephone);
+      .query(querysUsers.getCredentialByEmail);
 
-    if (existingUser.recordset.length > 0) {
-      return res.status(409).json({ msg: "Un usuario con ese correo ya existe" });
-    }
-    
-    if (existingUserTelephone.recordset.length > 0) {
-      return res.status(409).json({ msg: "Un usuario con ese teléfono ya existe" });
+    if (existingCredential.recordset.length > 0) {
+      return res.status(409).json({ msg: "El correo electrónico ya está registrado." });
     }
 
-    const hashedPassword = await bcrypt.hash(contrasena, 10);
+    // Hashear la contraseña antes de guardarla en la base de datos
+    const hashedPassword = await bcrypt.hash(contrasena, 10); // 10 es el costo de hash
 
-    await pool
+    // Insertar el nuevo usuario en la tabla Usuarios
+    const result = await pool
       .request()
       .input("nombre", sql.VarChar, nombre)
       .input("primerApellido", sql.VarChar, primerApellido)
       .input("segundoApellido", sql.VarChar, segundoApellido)
-      .input("fechaNacimiento", sql.Date, fechaNacimiento)
-      .input("genero", sql.VarChar, genero)
       .input("correoElectronico", sql.VarChar, correoElectronico)
-      .input("telefono", sql.BigInt, telefono)
-      .input("contraseña", sql.VarChar, hashedPassword) // Se almacena la contraseña hasheada
-      .input("direccion", sql.VarChar, direccion)
       .query(querysUsers.addNewUser);
 
-    res.json({ nombre, primerApellido, segundoApellido, fechaNacimiento, genero, correoElectronico, telefono, contrasena: hashedPassword, direccion });
+    // Obtener el ID del nuevo usuario insertado
+    const userId = result.recordset[0].ID_usuario;
+
+    // Insertar las credenciales del nuevo usuario en la tabla Credenciales
+    await pool
+      .request()
+      .input("ID_usuario", sql.Int, userId)
+      .input("correoElectronico", sql.VarChar, correoElectronico)
+      .input("contraseña", sql.VarChar, hashedPassword)
+      .query(querysUsers.addNewCredential);
+
+    res.json({ msg: "Usuario creado exitosamente." });
   } catch (error) {
     res.status(500).send(error.message);
   }
 };
-
-
 
 export const getUserById = async (req, res) => {
   try {
@@ -165,11 +370,12 @@ export const updatePasswordById = async (req, res) => {
   }
 
   try {
+    const hashedPassword = await bcrypt.hash(contraseña, 10); // Hashear la contraseña con un costo de 10
     const pool = await getConnection();
 
     await pool
       .request()
-      .input("contraseña", sql.VarChar, contraseña)
+      .input("contraseña", sql.VarChar, hashedPassword) // Pasar la contraseña hasheada en lugar de la original
       .input("IdUsuario", req.params.id)
       .query(querysUsers.updatePasswordById);
 
@@ -178,7 +384,6 @@ export const updatePasswordById = async (req, res) => {
     res.status(500).json({ success: false, error: error.message });
   }
 };
-
 export const login = async (req, res) => {
   const { correoElectronico, contraseña } = req.body;
 
@@ -187,19 +392,25 @@ export const login = async (req, res) => {
   }
   try {
     const pool = await getConnection();
-
     const result = await pool
       .request()
       .input("correoElectronico", sql.VarChar, correoElectronico)
-      .input("contraseña", sql.VarChar, contraseña)
-      .query(querysUsers.login);
+      .query(querysUsers.getUserByEmail);
 
     if (result.recordset.length === 0) {
       return res.status(401).json({ msg: "Correo electrónico o contraseña no válidos" });
     }
 
     const user = result.recordset[0];
+    const hashedPassword = user.contraseña; // Se obtiene la contraseña hasheada almacenada en la base de datos
 
+    const passwordMatch = await bcrypt.compare(contraseña, hashedPassword);
+    
+    if (!passwordMatch) {
+      return res.status(401).json({ msg: "Correo electrónico o contraseña no válidos" });
+    }
+
+    // Si llega aquí, la autenticación fue exitosa
     res.json(user);
   } catch (error) {
     res.status(500).json({ msg: "Error interno del servidor", error: error.message });
--- src/database/querys.js
+++ src/database/querys.js
@@ -1,10 +1,93 @@
+// export const querys = {
+//   getAllProducts: "SELECT * FROM Productos",
+//   getProductById: "SELECT * FROM Productos WHERE ID_producto = @IdProducto",
+//   addNewProduct: "INSERT INTO Productos (nombre, descripcion, precio, precioDescuento, existencias, ID_categoria, ID_subcategoria, ID_marca) VALUES (@nombre, @descripcion, @precio, @precioDescuento, @existencias, @ID_categoria, @ID_subcategoria, @ID_marca)",
+//   deleteProduct: "DELETE FROM Productos WHERE ID_producto = @IdProducto",
+//   getTotalProducts: "SELECT COUNT(*) FROM Productos",
+//   updateProductById: "UPDATE Productos SET nombre = @nombre, descripcion = @descripcion, precio = @precio, precioDescuento = @precioDescuento, existencias = @existencias, ID_categoria = @ID_categoria, ID_subcategoria = @ID_subcategoria, ID_marca = @ID_marca WHERE ID_producto = @IdProducto",
+//   getAllProductsWithRelations: `
+//   SELECT 
+//   P.ID_producto,
+//   P.nombre,
+//   P.descripcion,
+//   P.precio,
+//   P.precioDescuento,
+//   p.existencias,
+//   C.ID_categoria,
+//   C.nombre AS nombreCategoria,
+//   SC.ID_subcategoria,
+//   SC.nombre AS nombreSubcategoria,
+//   M.ID_marca,
+//   M.nombre AS nombreMarca
+//   FROM 
+//   Productos P
+//   INNER JOIN 
+//     CategoriasProductos C ON P.ID_categoria = C.ID_categoria
+//   INNER JOIN 
+//     SubcategoriasProductos SC ON P.ID_subcategoria = SC.ID_subcategoria
+//   INNER JOIN 
+//     Marcas M ON P.ID_marca = M.ID_marca
+//   `
+// };
+
+
+// export const querysUsers = {
+//   getAllUsers: "SELECT * FROM Usuarios",
+//   getUserById: "SELECT * FROM Usuarios WHERE ID_usuario = @IdUsuario",
+//   addNewUser: "INSERT INTO Usuarios (nombre, primerApellido, segundoApellido, direccion, correoElectronico, contraseña, telefono, fechaNacimiento, genero) VALUES (@nombre, @primerApellido, @segundoApellido, @direccion, @correoElectronico, @contraseña, @telefono, @fechaNacimiento, @genero);",
+//   deleteUser: "DELETE FROM Usuarios WHERE ID_usuario = @IdUsuario",
+//   getTotalUsers: "SELECT COUNT(*) FROM Usuarios",
+//   updateUserById: "UPDATE Usuarios SET nombre = @nombre, primerApellido = @primerApellido, segundoApellido = @segundoApellido, direccion = @direccion, correoElectronico = @correoElectronico, contraseña = @contraseña, telefono = @telefono, fechaNacimiento = @fechaNacimiento, genero = @genero WHERE ID_usuario = @IdUsuario",
+//   getUserByEmail: "SELECT * FROM Usuarios WHERE correoElectronico = @correoElectronico;",
+//   getUserByTelephone: "SELECT * FROM Usuarios WHERE telefono = @telefono;",
+//   updatePasswordById: "UPDATE Usuarios SET contraseña = @contraseña WHERE ID_usuario = @IdUsuario;",
+//   login: "SELECT * FROM Usuarios WHERE correoElectronico = @correoElectronico AND contraseña = @contraseña;",
+// };
+
+
+// export const querysToken = {
+//   addNewUserToken: "INSERT INTO Tokens (ID_usuario, token, expiracionToken) VALUES (@IdUser, @token, DATEADD(MINUTE, 30, GETDATE()));",
+//   getUserToken: "SELECT * FROM Tokens WHERE ID_usuario = @IdUser;",
+//   updateTokenById: "UPDATE Tokens SET token = @token, expiracionToken = DATEADD(MINUTE, 30, GETDATE()) WHERE ID_usuario = @IdUser;",
+//   deleteTokenById: "DELETE FROM Tokens WHERE ID_usuario = @IdUser;"
+// };
+
+
+// export const querysCategoriasProductos = {
+//   getAllCategoriasProductos: "SELECT * FROM CategoriasProductos",
+//   getCategoriaProductoById: "SELECT * FROM CategoriasProductos WHERE ID_categoria = @IdCategoria",
+//   addNewCategoriaProducto: "INSERT INTO CategoriasProductos (nombre) VALUES (@nombre);",
+//   deleteCategoriaProducto: "DELETE FROM CategoriasProductos WHERE ID_categoria = @IdCategoria",
+//   getTotalCategoriasProductos: "SELECT COUNT(*) FROM CategoriasProductos",
+//   updateCategoriaProductoById: "UPDATE CategoriasProductos SET nombre = @nombre WHERE ID_categoria = @IdCategoria"
+// };
+
+// export const querysSubcategoriasProductos = {
+//   getAllSubcategoriasProductos: "SELECT * FROM SubcategoriasProductos",
+//   getSubcategoriaProductoById: "SELECT * FROM SubcategoriasProductos WHERE ID_subcategoria = @IdSubcategoria",
+//   addNewSubcategoriaProducto: "INSERT INTO SubcategoriasProductos (nombre, ID_categoria) VALUES (@nombre, @IdCategoria);",
+//   deleteSubcategoriaProducto: "DELETE FROM SubcategoriasProductos WHERE ID_subcategoria = @IdSubcategoria",
+//   getTotalSubcategoriasProductos: "SELECT COUNT(*) FROM SubcategoriasProductos",
+//   updateSubcategoriaProductoById: "UPDATE SubcategoriasProductos SET nombre = @nombre, ID_categoria = @IdCategoria WHERE ID_subcategoria = @IdSubcategoria",
+//   getCategoriasByID_categoria: "SELECT * FROM SubcategoriasProductos WHERE ID_categoria = @IdCategoria"
+// };
+
+// export const querysMarcas = {
+//   getAllMarcas: "SELECT * FROM Marcas",
+//   getMarcaById: "SELECT * FROM Marcas WHERE ID_marca = @IdMarca",
+//   addNewMarca: "INSERT INTO Marcas (nombre, ID_categoria) VALUES (@nombre, @IdCategoria);",
+//   deleteMarca: "DELETE FROM Marcas WHERE ID_marca = @IdMarca",
+//   getTotalMarcas: "SELECT COUNT(*) FROM Marcas",
+//   updateMarcaById: "UPDATE Marcas SET nombre = @nombre, ID_categoria = @IdCategoria WHERE ID_marca = @IdMarca",
+//   getMarcasByID_marca:"SELECT * FROM Marcas WHERE ID_categoria = @IdCategoria"
+// };
 export const querys = {
   getAllProducts: "SELECT * FROM Productos",
   getProductById: "SELECT * FROM Productos WHERE ID_producto = @IdProducto",
-  addNewProduct: "INSERT INTO Productos (nombre, descripcion, precio, precioDescuento, existencias, ID_categoria, ID_subcategoria, ID_marca) VALUES (@nombre, @descripcion, @precio, @precioDescuento, @existencias, @ID_categoria, @ID_subcategoria, @ID_marca)",
+  addNewProduct: "INSERT INTO Productos (nombre, descripcion, precio, precioDescuento, ID_categoria, ID_subcategoria, ID_marca) VALUES (@nombre, @descripcion, @precio, @precioDescuento, @ID_categoria, @ID_subcategoria, @ID_marca)",
   deleteProduct: "DELETE FROM Productos WHERE ID_producto = @IdProducto",
   getTotalProducts: "SELECT COUNT(*) FROM Productos",
-  updateProductById: "UPDATE Productos SET nombre = @nombre, descripcion = @descripcion, precio = @precio, precioDescuento = @precioDescuento, existencias = @existencias, ID_categoria = @ID_categoria, ID_subcategoria = @ID_subcategoria, ID_marca = @ID_marca WHERE ID_producto = @IdProducto",
+  updateProductById: "UPDATE Productos SET nombre = @nombre, descripcion = @descripcion, precio = @precio, precioDescuento = @precioDescuento, ID_categoria = @ID_categoria, ID_subcategoria = @ID_subcategoria, ID_marca = @ID_marca WHERE ID_producto = @IdProducto",
   getAllProductsWithRelations: `
   SELECT 
   P.ID_producto,
@@ -12,7 +95,6 @@ export const querys = {
   P.descripcion,
   P.precio,
   P.precioDescuento,
-  p.existencias,
   C.ID_categoria,
   C.nombre AS nombreCategoria,
   SC.ID_subcategoria,
@@ -34,24 +116,29 @@ export const querys = {
 export const querysUsers = {
   getAllUsers: "SELECT * FROM Usuarios",
   getUserById: "SELECT * FROM Usuarios WHERE ID_usuario = @IdUsuario",
-  addNewUser: "INSERT INTO Usuarios (nombre, primerApellido, segundoApellido, direccion, correoElectronico, contraseña, telefono, fechaNacimiento, genero) VALUES (@nombre, @primerApellido, @segundoApellido, @direccion, @correoElectronico, @contraseña, @telefono, @fechaNacimiento, @genero);",
-  deleteUser: "DELETE FROM Usuarios WHERE ID_usuario = @IdUsuario",
+  addNewUser: `
+    INSERT INTO Usuarios (nombre, primerApellido, segundoApellido, fechaCreacion, ID_rol) 
+    VALUES (@nombre, @primerApellido, @segundoApellido, GETDATE(), 2);
+    SELECT SCOPE_IDENTITY() AS ID_usuario;
+  `,  deleteUser: "DELETE FROM Usuarios WHERE ID_usuario = @IdUsuario",
   getTotalUsers: "SELECT COUNT(*) FROM Usuarios",
   updateUserById: "UPDATE Usuarios SET nombre = @nombre, primerApellido = @primerApellido, segundoApellido = @segundoApellido, direccion = @direccion, correoElectronico = @correoElectronico, contraseña = @contraseña, telefono = @telefono, fechaNacimiento = @fechaNacimiento, genero = @genero WHERE ID_usuario = @IdUsuario",
-  getUserByEmail: "SELECT * FROM Usuarios WHERE correoElectronico = @correoElectronico;",
+  getUserByEmail: "SELECT * FROM Credenciales WHERE correoElectronico = @correoElectronico;",
   getUserByTelephone: "SELECT * FROM Usuarios WHERE telefono = @telefono;",
-  updatePasswordById: "UPDATE Usuarios SET contraseña = @contraseña WHERE ID_usuario = @IdUsuario;",
-  login: "SELECT * FROM Usuarios WHERE correoElectronico = @correoElectronico AND contraseña = @contraseña;",
+  updatePasswordById: "UPDATE Credenciales SET contraseña = @contraseña WHERE ID_usuario = @IdUsuario;",
+  login: "SELECT * FROM Credenciales WHERE correoElectronico = @correoElectronico AND contraseña = @contraseña;",
+  getCredentialByEmail: "SELECT * FROM Credenciales WHERE correoElectronico = @correoElectronico;",
+  addNewCredential: "INSERT INTO Credenciales (ID_usuario, correoElectronico, contraseña) VALUES (@ID_usuario, @correoElectronico, @contraseña);",
+
 };
 
 
 export const querysToken = {
-  addNewUserToken: "INSERT INTO Tokens (ID_usuario, token, expiracionToken) VALUES (@IdUser, @token, DATEADD(MINUTE, 30, GETDATE()));",
-  getUserToken: "SELECT * FROM Tokens WHERE ID_usuario = @IdUser;",
-  updateTokenById: "UPDATE Tokens SET token = @token, expiracionToken = DATEADD(MINUTE, 30, GETDATE()) WHERE ID_usuario = @IdUser;",
-  deleteTokenById: "DELETE FROM Tokens WHERE ID_usuario = @IdUser;"
+  addNewUserToken: "INSERT INTO RecuperacionContraseña (ID_usuario, CodigoRecuperacion) VALUES (@IdUser, @recovery_code);",
+  getUserToken: "SELECT * FROM RecuperacionContraseña WHERE ID_usuario = @IdUser;",
+  updateTokenById: "UPDATE RecuperacionContraseña SET CodigoRecuperacion = @recovery_code WHERE ID_usuario = @IdUser;",
+  deleteTokenById: "DELETE FROM RecuperacionContraseña WHERE ID_usuario = @IdUser;"
 };
-
 
 export const querysCategoriasProductos = {
   getAllCategoriasProductos: "SELECT * FROM CategoriasProductos",
