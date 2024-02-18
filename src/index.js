95763f737485f241699d13887618ea08e58af683
Dobledroid <carlosexequiel360@gmail.com>
2024-02-17 23:34:41 -0600

    index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
+// import app from "./app";
+
+// app.listen(app.get("port"));
+
+// console.log("Server on port", app.get("port"));
+
 import app from "./app";
 
-app.listen(app.get("port"));
+const PORT = process.env.PORT || 3001; // Puerto por defecto 3001
 
-console.log("Server on port", app.get("port"));
+app.listen(PORT, () => {
+  console.log(`Servidor en el puerto ${PORT}`);
+});
